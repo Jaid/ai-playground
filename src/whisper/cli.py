c2f645bf5969dcea5a1fb0718a92992057c75a31
Jaid <6216144+Jaid@users.noreply.github.com>
2024-02-21 01:27:56 +0100

    Improved code
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/whisper/cli.py
+++ src/whisper/cli.py
@@ -1,41 +1,100 @@
+from math import trunc
 import torch
-from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-import ruamel.yaml
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline, AutomaticSpeechRecognitionPipeline
 import argparse
 from pathlib import Path
-import srt
+import time
+import subprocess
+from rich.console import Console
 
+console = Console(record=True, soft_wrap=True)
 parser = argparse.ArgumentParser()
 parser.add_argument('input_file', type=str)
-parser.add_argument('--output-file', type=str, default='out/whisper/*.yml')
+parser.add_argument('--batch-size', type=int, default=1)
+parser.add_argument('--chunk-length', type=int, default=60)
+parser.add_argument('--force-cpu', action='store_true')
+parser.add_argument('--force-cuda', action='store_true')
 parser.add_argument('--language', type=str)
+parser.add_argument('--low-memory', action='store_true')
+parser.add_argument('--max-new-tokens', type=int)
+parser.add_argument('--model', type=str, default='openai/whisper-large-v3')
+parser.add_argument('--output-file', type=str)
+parser.add_argument('--print-text', action='store_true')
+parser.add_argument('--resample-quality', type=int, default=28)
 parser.add_argument('--word-accuracy', action='store_true')
 args = parser.parse_args()
-isCuda = torch.cuda.is_available() #and not args.word_accuracy
-device = isCuda and 'cuda' or 'cpu'
-torchDtype = isCuda and torch.float16 or torch.float32
-modelId = 'openai/whisper-large-v3'
+console.out('Args:', args)
+startTime = time.time()
+preparedInputFile = 'out/whisper/input.flac'
+result = subprocess.run([
+  'ffprobe',
+  '-v',
+  'error',
+  '-show_entries',
+  'format=duration',
+  '-of',
+  'default=noprint_wrappers=1:nokey=1',
+  args.input_file
+], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+inputDuration = float(result.stdout)
+console.out('Input duration:', inputDuration, 'seconds')
+subprocess.run([
+  'ffmpeg',
+  '-hide_banner',
+  '-strict',
+  'experimental',
+  '-hwaccel',
+  'auto',
+  '-y',
+  '-i',
+  args.input_file,
+  '-af',
+  f'aresample=resampler=soxr:precision={args.resample_quality}:osr=16000',
+  '-ac',
+  '1',
+  preparedInputFile
+], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+console.out(f'Prepared input file {args.input_file} in {time.time() - startTime:.2f} seconds')
+startTime = time.time()
+if args.force_cuda:
+  isCuda = True
+elif args.force_cpu:
+  isCuda = False
+else:
+  isCuda = torch.cuda.is_available()
+if isCuda:
+  device = 'cuda'
+  torchDtype = torch.float16
+else:
+  device = 'cpu'
+  torchDtype = torch.float32
+if isCuda and not args.word_accuracy:
+  attentionStrategy = 'flash_attention_2'
+else:
+  attentionStrategy = 'eager'
 modelLoadingOptions = {
   'torch_dtype': torchDtype,
-  'low_cpu_mem_usage': True,
   'use_safetensors': True,
-  'attn_implementation': (isCuda and not args.word_accuracy) and 'flash_attention_2' or 'eager'
+  'attn_implementation': attentionStrategy
 }
+if args.low_memory:
+  modelLoadingOptions['low_cpu_mem_usage'] = True
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
-  modelId,
+  args.model,
   **modelLoadingOptions
 )
 model.to(device)
-processor = AutoProcessor.from_pretrained(modelId)
+processor = AutoProcessor.from_pretrained(args.model)
+console.out('Processor:', processor)
 returnTimestamps = 'word' if args.word_accuracy else True
-pipe = pipeline(
+pipe: AutomaticSpeechRecognitionPipeline = AutomaticSpeechRecognitionPipeline(
   'automatic-speech-recognition',
   model=model,
   tokenizer=processor.tokenizer,
   feature_extractor=processor.feature_extractor,
-  max_new_tokens=128,
-  chunk_length_s=30,
-  batch_size=16,
+  max_new_tokens=args.max_new_tokens or args.chunk_length * 5,
+  chunk_length_s=args.chunk_length,
+  batch_size=args.batch_size,
   return_timestamps=returnTimestamps,
   torch_dtype=torchDtype,
   device=device,
@@ -43,27 +102,65 @@ pipe = pipeline(
 inferenceOptions = {}
 if args.language:
   inferenceOptions['language'] = args.language
+console.out(f'Prepared model in {round(time.time() - startTime, 2)} seconds')
+startTime = time.time()
+def data():
+    while True:
+        # This could come from a dataset, a database, a queue or HTTP request
+        # in a server
+        # Caveat: because this is iterative, you cannot use `num_workers > 1` variable
+        # to use multiple threads to preprocess data. You can still have 1 thread that
+        # does the preprocessing while the main runs the big inference
+        yield "This is a test"
 result = pipe(
-  args.input_file,
+
+  preparedInputFile,
   generate_kwargs=inferenceOptions
 )
-ruamel=ruamel.yaml.YAML()
+inferenceDuration = time.time() - startTime
+inferenceSpeed = inputDuration / inferenceDuration * 100
+console.out(f'Inference done in {round(inferenceDuration, 2)} seconds ({trunc(inferenceSpeed)}% speed)')
 inputFileStem = Path(args.input_file).stem
-outputFile = args.output_file.replace('*', inputFileStem)
-outputFolder = Path(outputFile).parent
-outputFolder.mkdir(parents=True, exist_ok=True)
-outputFileExtension = Path(args.output_file).suffix[1:]
-with open(outputFile, 'w') as file:
-  if outputFileExtension == 'yml':
-    ruamel.dump(result, file)
-  if outputFileExtension == 'srt':
-    srtEntries = []
-    for i, chunk in enumerate(result['chunks']):
-      srtEntries.append(srt.Subtitle(
-        index=i + 1,
-        start=srt.timedelta(seconds=chunk['timestamp'][0]),
-        end=srt.timedelta(seconds=chunk['timestamp'][1]),
-        content=chunk['text'].strip()
-      ))
-    fileContent = srt.compose(srtEntries)
-    file.write(fileContent)
+outputs = {}
+if args.output_file:
+  outputFile = args.output_file.replace('*', inputFileStem)
+  outputFolder = Path(outputFile).parent
+  outputFileExtension = Path(args.output_file).suffix[1:]
+  outputs[outputFileExtension] = outputFile
+else:
+  outputFolder = 'out/whisper'
+  outputs['yml'] = f'{outputFolder}/{inputFileStem}.yml'
+  outputs['srt'] = f'{outputFolder}/{inputFileStem}.srt'
+Path(outputFolder).mkdir(parents=True, exist_ok=True)
+for outputFileExtension, outputFile in outputs.items():
+  console.out('Writing', outputFileExtension, 'to', Path(outputFile))
+  with open(outputFile, 'w') as file:
+    if outputFileExtension == 'yml':
+      import ruamel.yaml
+      ruamel=ruamel.yaml.YAML()
+      ruamel.dump(result, file)
+    if outputFileExtension == 'srt':
+      import srt
+      srtEntries = []
+      for i, chunk in enumerate(result['chunks']):
+        givenStart = chunk['timestamp'][0]
+        if givenStart == None or givenStart < 0:
+          start = 0
+        else:
+          start = givenStart
+        givenEnd = chunk['timestamp'][1]
+        if givenEnd == None or givenEnd < inputDuration:
+          end = inputDuration
+        else:
+          end = givenEnd
+        srtEntries.append(srt.Subtitle(
+          index=i + 1,
+          start=srt.timedelta(seconds=start),
+          end=srt.timedelta(seconds=end),
+          content=chunk['text'].strip()
+        ))
+      fileContent = srt.compose(srtEntries)
+      file.write(fileContent)
+if args.print_text:
+  console.out(result['text'].strip())
+console.save_text('out/whisper/console.log', styles=True)
